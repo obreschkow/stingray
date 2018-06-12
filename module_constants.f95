68d4c575fa8eaf66730ae8e52376afb24ac5d383
obreschkow <danail.obreschkow@gmail.com>
2018-06-12 13:07:16 +0800

    Update

--- module_constants.f95
+++ module_constants.f95
@@ -4,7 +4,7 @@ module module_constants
 
    public
    
-   character(*),parameter  :: version = '0.1'
+   character(*),parameter  :: version = '0.2'
    
    ! Universal constants
    real*4,parameter        :: pi = 3.14159265359
